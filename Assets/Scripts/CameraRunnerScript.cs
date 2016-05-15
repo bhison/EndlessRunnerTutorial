ea7ae8a866f14b1d4ae6b6945d21c727991cf86d
bhison <catbat04@gmail.com>
2016-05-15 23:54:56 +0100

    Half the tutorial done
    No powers
    https://unity3d.com/learn/tutorials/modules/beginner/live-training-archi
    ve/infinite-runner

---
+++ Assets/Scripts/CameraRunnerScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRunnerScript : MonoBehaviour {
+
+	public Transform player;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (player == null)
+			return;
+		transform.position = new Vector3 (player.position.x + 6, 0, -10);	
+	}
+}
